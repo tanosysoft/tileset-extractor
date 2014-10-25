40ca5507c68d2af861a699caa9ebff1fba4fae0e
Guilherme Vieira <super.driver.512@gmail.com>
2014-10-25 10:22:51 -0200

    New syntax coding standards.

--- analyzer.js
+++ analyzer.js
@@ -16,14 +16,11 @@
 */
 'use strict';
 window.tilesetting = window.tilesetting || {};
-function is_integer(number)
-{
+function is_integer(number) {
 	return (number % 1 === 0);
 }
-tilesetting.analyze_image = function(image, canvas, options, callback)
-{
-	if(!callback)
-	{
+tilesetting.analyze_image = function(image, canvas, options, callback) {
+	if(!callback) {
 		callback = options;
 		options = {};
 	}
@@ -31,64 +28,68 @@ tilesetting.analyze_image = function(image, canvas, options, callback)
 	var log_progress = options.log_progress || true;
 	var log_errors = options.log_errors || true;
 	var hashes_per_event_loop = options.hashes_per_event_loop || 4;
-	try
-	{
+	var context;
+	var analysis;
+	try {
 		canvas.width = image.width;
 		canvas.height = image.height;
-		var context = canvas.getContext('2d');
+		context = canvas.getContext('2d');
 		context.drawImage(image, 0, 0);
-		var analysis =
-		{
-			tiles_by_coordinates: {},
-			tiles_by_hash: {},
+		analysis = {
 			tile_size: tile_size
+			, tileset: {}
+			, tilemap: {
+				width: canvas.width / tile_size
+				, height: canvas.height / tile_size
+				, tiles: {}
+			}
 		};
-		analysis.map_width_in_tiles = canvas.width / tile_size;
-		analysis.map_height_in_tiles = canvas.height / tile_size;
-		if(!is_integer(analysis.map_width_in_tiles) || !is_integer(analysis.map_height_in_tiles))
-		{
-			throw new Error("Map width / height must be multiples of tile size.");
+		if (
+			!is_integer(analysis.tilemap.width)
+			|| !is_integer(analysis.tilemap.height)
+		) {
+			throw new Error("Map width and height must be multiples of tile size.");
 		}
-		var map_area_in_tiles = analysis.map_width_in_tiles * analysis.map_height_in_tiles;
-		var i = 0;
-		var interval = setInterval
-		(
-			function()
-			{
-				if(log_progress && i % 32 === 0)
-				{
-					console.log("Hashing tile ", i, "out of", map_area_in_tiles, "...");
-				}
-				do
-				{
-					var x = i % analysis.map_width_in_tiles;
-					var y = Math.floor(i / analysis.map_width_in_tiles);
-					var tile_image_data = context.getImageData
-					(
-						x * tile_size,
-						y * tile_size,
-						tile_size,
-						tile_size
-					);
-					var tile_data = tile_image_data.data;
-					var tile_hash = SparkMD5.ArrayBuffer.hash(tile_data);
-					analysis.tiles_by_coordinates[[x, y].join('/')] = { hash: tile_hash };
-					analysis.tiles_by_hash[tile_hash] = { x: x, y: y };
-					if(++i === map_area_in_tiles)
-					{
-						clearInterval(interval);
-						callback(null, analysis);
-						break;
+		(function analyze_hashes() {
+			var i = 0;
+			var map_area_in_tiles = analysis.map_width_in_tiles * analysis.map_height_in_tiles;
+			var interval = setInterval (
+				function() {
+					var x;
+					var y;
+					var tile_hash;
+					if(log_progress && i % 32 === 0) {
+						console.log("Hashing tile ", i, "out of", map_area_in_tiles, "...");
 					}
-				} while(i % hashes_per_event_loop !== 0);
-			},
-			1
-		);
+					do {
+						x = i % analysis.map_width_in_tiles;
+						y = Math.floor(i / analysis.map_width_in_tiles);
+						tile_hash = SparkMD5.ArrayBuffer.hash (
+							context.getImageData (
+								x * tile_size
+								, y * tile_size
+								, tile_size
+								, tile_size
+							)
+							.data
+						);
+						analysis.tileset[tile_hash] = { x: x, y: y };
+						analysis.tilemap[x + '/' + y] = { hash: tile_hash };
+						if(++i === map_area_in_tiles) {
+							clearInterval(interval);
+							callback(null, analysis);
+							break;
+						}
+					} while (
+						i % hashes_per_event_loop !== 0
+					);
+				},
+				1
+			);
+		})();
 	}
-	catch(error)
-	{
-		if(log_errors)
-		{
+	catch(error) {
+		if(log_errors) {
 			console.error(error);
 		}
 		callback(error);
